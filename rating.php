17a08273714d53b37ed605f3622315d35db2650d
bose <yrmewara@student.sfit.ac.in>
2023-09-21 21:26:55 +0530

    initial commit

---
+++ rating.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="x-icon" href="./images/lcon.png">
+    <title>Google Rating</title>
+    <style>
+   .home-button {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #333;
+      color: #fff;
+      text-decoration: none;
+      border-radius: 5px;
+   }
+
+   .home-button:hover {
+      background-color: #555;
+   }
+</style>
+</head>
+<body>
+<a href="home.php" class="home-button">Home</a>
+
+<script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+<div class="elfsight-app-3b9b9ea2-b9a2-45e1-a493-0b0d90064e50"></div>
+
+    
+</body>
+</html>
